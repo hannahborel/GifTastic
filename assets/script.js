ee0e719f17e89c957efed11c086095f43eb1d1cd
Hannah Borel <hannahborel>
2020-09-12 12:09:27 -0500

    Printing json data object to console sucessfull.

--- assets/script.js
+++ assets/script.js
@@ -2,17 +2,35 @@ function sendApiRequest(){
     var userInput = document.getElementById("input").value
     console.log(userInput)
     
-    var giphyApiKey = "12pav36GYvNLT6Hz68imiF0j0Q7RiiWH"
-    var giphyApiURL = "https://api.giphy.com/v1/gifs.search?q=${userInput}&rating=g&api_key=${giphyApiKey}"
+    var APIKEY = "12pav36GYvNLT6Hz68imiF0j0Q7RiiWH";
+    var URL = `https://api.giphy.com/v1/gifs/search?q=${userInput}&api_key=${APIKEY}&rating=g&limit=10`
 
-    fetch(giphyApiURL).then(function(data){
-        return data.json()
-    })
-    .then(function(json){
-        console.log(json.data[0].images.fixed_height.url)
-        var imgPath = jsoon.data[0].images.fixed_height.url
-        var img = document.createElement("img")
-        img.setAttribute("src", imgPath)
-        document.body.appendChild(img)
-    })
-}
+
+
+    fetch(URL)
+        .then((res) => {
+            res.json()
+        })
+        .then((giphyOBJ) => {
+            console.log(giphyOBJ.data)
+        })
+        .catch(err =>{
+            console.log(err)
+        })
+    };
+
+
+
+
+
+
+
+
+
+    // .then(function(json){
+    //     console.log(json.data[0].images.fixed_height.url)
+    //     var imgPath = json.data[0].images.fixed_height.url
+    //     var img = document.createElement("img")
+    //     img.setAttribute("src", imgPath)
+    //     document.body.appendChild(img)
+    // })
